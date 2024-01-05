12887561c0b6accec72f2fbd5411289caa4857c1
yoonminhyeok <116967490+yoonminhyeok@users.noreply.github.com>
2024-01-05 17:01:41 +0900

    Add files via upload

---
+++ 그림판 프로젝트(only front-end/js/app.js
@@ -0,0 +1,144 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+
+const INITIAL_COLOR = "#000000";
+const CANVAS_SIZE = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+ctx.lineWidth = 2.5; /* 라인 굵기 */
+
+let painting = false;
+let filling = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function startPainting() {
+  painting = true;
+}
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+function handleCreateObject(event) {
+  event.preventDefault();
+
+  const canvas = document.querySelector("#canvas > .wrapper");
+  const palette = document.getElementById("palette_form");
+  const object_state = palette.querySelectorAll(
+    'input[type="text"], input[type="radio"]:checked'
+  );
+
+  // 도형 설정값을 JSON형태로 치환.
+  const settings = Array.from(object_state).reduce(function (prev, crnt, idx) {
+    if (idx === 1) {
+      const _setting = {};
+      _setting[prev.name] = prev.value;
+      _setting[crnt.name] = crnt.value;
+      return _setting;
+    }
+    prev[crnt.name] = crnt.value;
+
+    return prev;
+  });
+
+  // 도형 생성
+  const object = createObject(settings);
+
+  // 도형을 도화지에 추가
+  canvas.appendChild(object);
+
+  // 도형이 텍스트인 경우, 바로 글을 작성할 수 있도록 포커싱.
+  if (settings.shape === "text") {
+    object.focus();
+  }
+}
+
+// 우클릭 방지
+/*
+function handleCM(event) {
+   event.preventDefault();
+ }
+ */
+
+function handleSaveClick() {
+  const image = canvas.toDataURL("image/png");
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS[EXPORT]";
+  link.click();
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
+  // canvas.addEventListener("contextmenu", handleCM);
+}
+
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColorClick)
+);
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
+}
